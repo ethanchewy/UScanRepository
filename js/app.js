a85cf80be7fbbd2f5c8cde921a662ac5b092cff7
Ethan Chiu <ethanchewy@users.noreply.github.com>
2015-03-14 02:43:26 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-$(document).ready(function() {	
-	var mySpreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=11x3_5BinmTr4sxeH4TiQrgvwp6OPKmERSOSuFrTPTH4#gid=0';
+var mySpreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=11x3_5BinmTr4sxeH4TiQrgvwp6OPKmERSOSuFrTPTH4#gid=0';
 	$('#statistics').sheetrock({
 	  url: mySpreadsheet
 	});
-});
-
