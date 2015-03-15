23e2119e5285468d3a680d4d08c5fccf5b214570
Ethan Chiu <ethanchewy@users.noreply.github.com>
2015-03-14 23:50:53 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,4 +15,15 @@ $('#runing_low').sheetrock({
   chunkSize: 10
 });
 
+$('.bar2').click(function() {
+    $("tablez").animate({
+      left: "0px"
+    }, 200);
+
+    $("tablez").animate({
+      left: "285px"
+    }, 200);
+  });
+  
+};
 
