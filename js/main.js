fc474726cf848dad9e773d31d4d300c7fbce8b40
Ethan Chiu <ethanchewy@users.noreply.github.com>
2015-03-14 23:43:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+var main = function() {
+ $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+  
+};
+$(document).ready(main);
