a8e11985163b41ba4cab25839777dcb0bbbd77c7
Ethan Chiu <ethanchewy@users.noreply.github.com>
2015-03-14 22:24:44 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // Define spreadsheet URL.
-var mySpreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=11x3_5BinmTr4sxeH4TiQrgvwp6OPKmERSOSuFrTPTH4#gid=0';
+var mySpreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=1G2xjX66QgIro6B9yV3A9E5CNINHYAp2gFjrXnfOdjHw#gid=0';
+
+// real sheet => https://docs.google.com/spreadsheet/ccc?key=11x3_5BinmTr4sxeH4TiQrgvwp6OPKmERSOSuFrTPTH4#gid=0
+// testing sheet => https://docs.google.com/spreadsheet/ccc?key=1G2xjX66QgIro6B9yV3A9E5CNINHYAp2gFjrXnfOdjHw#gid=0
 
 // Load an entire sheet.
 $('#statistics').sheetrock({
