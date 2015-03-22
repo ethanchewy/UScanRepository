7ebd24cdf843b96a1e65ca2aca041e75e0c8a4f1
Ethan Chiu <ethanchewy@users.noreply.github.com>
2015-03-22 02:14:37 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,15 +11,19 @@ $(document).ready(function() {
 
 
 // Define spreadsheet URL.
-var mySpreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=1G2xjX66QgIro6B9yV3A9E5CNINHYAp2gFjrXnfOdjHw#gid=0';
-
-// real sheet => https://docs.google.com/spreadsheet/ccc?key=11x3_5BinmTr4sxeH4TiQrgvwp6OPKmERSOSuFrTPTH4#gid=0
+var mySpreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=11x3_5BinmTr4sxeH4TiQrgvwp6OPKmERSOSuFrTPTH4#gid=0';
+var myDataAnalysis = "https://docs.google.com/spreadsheet/ccc?key=11x3_5BinmTr4sxeH4TiQrgvwp6OPKmERSOSuFrTPTH4#gid=1813653491"// real sheet => https://docs.google.com/spreadsheet/ccc?key=11x3_5BinmTr4sxeH4TiQrgvwp6OPKmERSOSuFrTPTH4#gid=0
 // testing sheet => https://docs.google.com/spreadsheet/ccc?key=1G2xjX66QgIro6B9yV3A9E5CNINHYAp2gFjrXnfOdjHw#gid=0
 
 
 // Load an entire sheet.
 $('#statistics').sheetrock({
   url: mySpreadsheet,
+  rowHandler: formatRows
+});
+
+$('#DataAnalysis').sheetrock({
+  url: myDataAnalysis,
   rowHandler: formatRows
 });
 
