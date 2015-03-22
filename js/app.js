bec9d1eac53747760a02a07f70c5d6e3ff09d88d
Ethan Chiu <ethanchewy@users.noreply.github.com>
2015-03-22 01:21:35 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ var formatRows = function (options) {
   var columnNumber = 2;
   var threshhold = 5;
 
-  $('tr', options.target).each(function (i, el) {
+  $('td', options.target).each(function (i, el) {
 
     var $tableRow = $(el);
     var amount = $('td:nth-child(' + columnNumber + ')', $tableRow).text();
